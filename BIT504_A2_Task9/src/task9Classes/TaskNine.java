5fc346807dc0608194075995a0e14d8e1ae6739d
NickBaine <nickbaine@gmail.com>
2025-10-28 11:41:29 +1300

    Initial project

---
+++ BIT504_A2_Task9/src/task9Classes/TaskNine.java
@@ -0,0 +1,13 @@
+package task9Classes;
+
+public class TaskNine {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is for the Task9 activity");
+		
+		System.out.println("\nI have to commit two changes to new branches");
+
+	}
+
+}
